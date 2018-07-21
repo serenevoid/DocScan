f2e2ac64eb47716d1af84af29062a13500ba0673
ParanoidWolf <ajuph9224@gmail.com>
2018-07-21 09:23:19 +0530

    Adaptive Gaussian Thresholding

---
+++ thrshld.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('test.jpg',0)
+#img = cv.medianBlur(img,5)
+th = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+plt.imshow(th, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
