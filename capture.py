e53efcdb72149a72cf6272d3523858f504e64197
ParanoidWolf <ajuph9224@gmail.com>
2018-07-30 22:23:52 +0530

    Capture.py

--- capture.py
+++ capture.py
@@ -1,23 +1,30 @@
-# Video Stream from camera
 import cv2
-import numpy as np
-cap = cv2.VideoCapture(0)
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame  = cap.read()
-	# Detect corners
-	frame = np.float32(frame)
-	corners = cv2.goodFeatureToTrack(frame, 100, 0.01, 10)
-	corners = np.int0(corners)
-	for corner in corners:
-		x,y = corner.ravel()
-		cv2.circle(frame, (x,y), 3, 255, -1)
-	# Show colour image output
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		# quit when 'q' is pressed
-		break
-# releasing capture when finished
-cap.release()
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Show the Doc")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
 cv2.destroyAllWindows()
+
